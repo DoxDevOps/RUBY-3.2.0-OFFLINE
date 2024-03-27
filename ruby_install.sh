7c6633a09564c7bf2b397de778b101e3c3d90bba
mnaboti <mnaboti@pedaids.org>
2024-03-27 14:58:55 +0200

    modified the installation script

--- ruby_install.sh
+++ ruby_install.sh
@@ -3,14 +3,14 @@
 # Step 1: Extract the file
 sudo tar -xvf ruby-3.2.0.b2z
 
-# Step 2: Copy Ruby to RVM directory if not exists
+# Step 2: Copy Ruby to RVM directory if it does not exist
 if [ ! -d "/usr/share/rvm/rubies/ruby-3.2.0" ]; then
     sudo cp -r ruby-3.2.0 /usr/share/rvm/rubies/
 else
     echo "Ruby directory already exists in RVM directory. Skipping copy...."
 fi
 
-# Step 3: Copy Ruby directory to ~/.rvm/wrappers if not exists
+# Step 3: Copy Ruby directory to ~/.rvm/wrappers if it does not exist
 if [ ! -d "$HOME/.rvm/wrappers/ruby-3.2.0" ]; then
     cp -r ruby-3.2.0 "$HOME/.rvm/wrappers"
 else
